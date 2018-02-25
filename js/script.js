a6fe398f0b69d323eb39129716bb6a0a60be5b25
ibremseth <idbremseth@gmail.com>
2018-02-25 17:33:42 -0600

    added getting data from google spreadsheet

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 
       google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawAllCharts);
+      google.charts.setOnLoadCallback(drawChart);
 
-      function drawAllCharts() {
-        drawAccelerationChart();
-        drawPositionChart();
-        drawGasPresChart();
-        drawOilTempChart();
-        drawCVTChart();
-        drawForceChart();
-        drawBrakePresChart();
-        drawShockDispChart();
-        drawSteeringDispChart();
+      function drawChart() {
+        var query = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1_JFqEI5z02JNDeSxHtCMOSUvBpxwxG5ubsNEc0KuKyM/edit?usp=sharing");
+        query.send(handleQueryResponse);
+      }
+      
+      function handleQueryResponse(response) {
+        if (response.isError()) {
+          console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+          return;
+        }
+
+        var data = response.getDataTable();
+        drawAccelerationChart(data);
+        drawPositionChart(data);
+        drawGasPresChart(data);
+        drawOilTempChart(data);
+        drawCVTChart(data);
+        drawForceChart(data);
+        drawBrakePresChart(data);
+        drawShockDispChart(data);
+        drawSteeringDispChart(data);
       }
 
-      function drawAccelerationChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawAccelerationChart(data) {
         var options = {
           title: 'Acceleration Chart',
           curveType: 'function',
@@ -32,14 +36,7 @@
       }
 
             
-      function drawPositionChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawPositionChart(data) {
         var options = {
           title: 'Position Chart',
           curveType: 'function',
@@ -49,14 +46,7 @@
         chart.draw(data, options);
       }
             
-      function drawGasPresChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawGasPresChart(data) {
         var options = {
           title: 'Gas Pedal Pressure Chart',
           curveType: 'function',
@@ -66,14 +56,7 @@
         chart.draw(data, options);
       }
             
-      function drawOilTempChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawOilTempChart(data) {
         var options = {
           title: 'Oil Tempuratue Chart',
           curveType: 'function',
@@ -83,14 +66,7 @@
         chart.draw(data, options);
       }
             
-      function drawCVTChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawCVTChart(data) {
         var options = {
           title: 'CVT Chart',
           curveType: 'function',
@@ -100,14 +76,7 @@
         chart.draw(data, options);
       }
             
-      function drawForceChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawForceChart(data) {
         var options = {
           title: 'Force Chart',
           curveType: 'function',
@@ -117,14 +86,7 @@
         chart.draw(data, options);
       }
             
-      function drawBrakePresChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawBrakePresChart(data) {
         var options = {
           title: 'Brake Pressue Chart',
           curveType: 'function',
@@ -134,14 +96,7 @@
         chart.draw(data, options);
       }
             
-      function drawShockDispChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawShockDispChart(data) {
         var options = {
           title: 'Shock Displacement Chart',
           curveType: 'function',
@@ -151,14 +106,7 @@
         chart.draw(data, options);
       }
             
-      function drawSteeringDispChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);
+      function drawSteeringDispChart(data) {
         var options = {
           title: 'Steering Displacment Chart',
           curveType: 'function',
