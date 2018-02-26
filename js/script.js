41109fb3171a1fdbd1d29d5e414a9ecf436658fc
ibremseth <idbremseth@gmail.com>
2018-02-25 18:28:10 -0600

    added getting data from different pages on the doc

--- js/script.js
+++ js/script.js
@@ -1,118 +1,205 @@
 
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawCharts);
 
-      function drawChart() {
-        var query = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1_JFqEI5z02JNDeSxHtCMOSUvBpxwxG5ubsNEc0KuKyM/edit?usp=sharing");
-        query.send(handleQueryResponse);
-      }
-      
-      function handleQueryResponse(response) {
-        if (response.isError()) {
-          console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-          return;
-        }
+var pages = ['&sheet=acceleration', '&sheet=position', '&sheet=gas-pres', '&sheet=oil-temp', '&sheet=cvt', '&sheet=force', '&sheet=brake-pres', '&sheet=shock-disp', '&sheet=steering-disp'];
+var url = 'https://docs.google.com/spreadsheets/d/1_JFqEI5z02JNDeSxHtCMOSUvBpxwxG5ubsNEc0KuKyM/edit?usp=sharing';
 
-        var data = response.getDataTable();
-        drawAccelerationChart(data);
-        drawPositionChart(data);
-        drawGasPresChart(data);
-        drawOilTempChart(data);
-        drawCVTChart(data);
-        drawForceChart(data);
-        drawBrakePresChart(data);
-        drawShockDispChart(data);
-        drawSteeringDispChart(data);
-      }
+function drawCharts() {
+    var query = new google.visualization.Query(url+pages[0]);
+    query.send(handleQueryResponseAcceleration);
 
-      function drawAccelerationChart(data) {
-        var options = {
-          title: 'Acceleration Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('acceleration-chart'));
-        chart.draw(data, options);
-      }
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponsePosition);
 
-            
-      function drawPositionChart(data) {
-        var options = {
-          title: 'Position Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('position-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawGasPresChart(data) {
-        var options = {
-          title: 'Gas Pedal Pressure Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('gas-pre-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawOilTempChart(data) {
-        var options = {
-          title: 'Oil Tempuratue Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('oil-temp-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawCVTChart(data) {
-        var options = {
-          title: 'CVT Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('cvt-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawForceChart(data) {
-        var options = {
-          title: 'Force Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('force-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawBrakePresChart(data) {
-        var options = {
-          title: 'Brake Pressue Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('brake-pre-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawShockDispChart(data) {
-        var options = {
-          title: 'Shock Displacement Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('shock-disp-chart'));
-        chart.draw(data, options);
-      }
-            
-      function drawSteeringDispChart(data) {
-        var options = {
-          title: 'Steering Displacment Chart',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('steering-disp-chart'));
-        chart.draw(data, options);
-      }
-        
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseGasPres);
+    
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseOilTemp);
+    
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseCVT);
+    
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseForce);
+    
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseBrakePres);
+    
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseShockDisp);
+    
+    var query = new google.visualization.Query(url+pages[1]);
+    query.send(handleQueryResponseSteeringDisp);
+}
+
+function handleQueryResponseAcceleration(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Acceleration Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+    
+    var id = 'acceleration-chart';
+    
+    drawChart(data, options, id);
+}
+
+function handleQueryResponsePosition(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Position Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'position-chart'
+    drawChart(data, options, id);
+}
+
+function handleQueryResponseGasPres(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Gas Pedal Pressure Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'gas-pre-chart';
+    drawChart(data, options, id);
+}
+
+function handleQueryResponseOilTemp(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Oil Tempuratue Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'oil-temp-chart';
+    drawChart(data, options, id);
+}
+
+function handleQueryResponseCVT(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'CVT Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'cvt-chart';
+    drawChart(data, options, id);
+}
+
+function handleQueryResponseForce(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Force Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'force-chart';
+    drawChart(data, options, id);
+}
+
+function handleQueryResponseBrakePres(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Brake Pressue Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'brake-pre-chart';
+    drawChart(data, options, id);
+}
+
+
+function handleQueryResponseShockDisp(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Shock Displacement Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'shock-disp-chart';
+    drawChart(data, options, id);
+}
+
+
+function handleQueryResponseSteeringDisp(response) {
+    if (response.isError()) {
+        console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+
+    var options = {
+        title: 'Steering Displacment Chart',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var data = response.getDataTable();
+
+    var id = 'steering-disp-chart';
+    drawChart(data, options, id);
+}
+
+function drawChart(data, options, id) {
+    var chart = new google.visualization.LineChart(document.getElementById(id));
+    chart.draw(data, options);
+}
