a0879429abaa6a300f578e4a2bae818c65960d14
ibremseth <idbremseth@gmail.com>
2018-03-25 19:37:02 -0500

    working on choose file

--- js/getfile.js
+++ js/getfile.js
@@ -24,3 +24,19 @@ var promise = $.getJSON( url, function( data, status){
 
 });
 }
+
+function printdata() {
+    //https://stackoverflow.com/questions/18116152/how-do-i-get-a-file-list-for-a-google-drive-public-hosted-folder
+    var folderId = "1omLjX3A8M-YWrUu4Q4tGlCKmZJyN7tFfp4DNJk-yEZ0";
+    //var url = "https://docs.google.com/spreadsheets/export?id=1omLjX3A8M-YWrUu4Q4tGlCKmZJyN7tFfp4DNJk-yEZ0&exportFormat=csv";
+    var url = "https://www.googleapis.com/drive/v3/files/" + folderId + "/export?mimeType=text&key=" + API_KEY;
+    var promise = $.getJSON( url, function( data, status){
+        // on success
+    });
+        promise.done(function( data ){
+        console.log(data);
+    }).fail(function(){
+    
+    });
+
+}
